1d0c501cc910302926cc68be34ec2634decf40c7
LucasKCP <77678913+LucasKCP@users.noreply.github.com>
2023-10-27 23:14:41 +0800

    update some part of app first

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,11 +1,26 @@
 import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {useState} from "react";
 
 export default function App() {
+    const [enteredGoalText, setGoalText] = useState('');
+    function goalInputHandler(inputText) {
+        console.log(inputText);
+        setGoalText(inputText)
+    }
+    function addGoalHandler() {
+        console.log(enteredGoalText)
+    }
+
     return (
         <View style={styles.container}>
-            <View>
-                <TextInput placeholder="Ur goal!!"/>
-                <Button title="Add Goal now mdfk!!"/>
+            <View style={styles.inputContainer}>
+                <TextInput placeholder="Ur goal!!"
+                           style={styles.textInput}
+                           onChangeText={goalInputHandler}
+                />
+                <Button title="Add Goal now mdfk!!"
+                        onPress={addGoalHandler}
+                />
             </View>
             <View>
                 <Text>Ur Fking goal</Text>
@@ -18,4 +33,19 @@ const styles = StyleSheet.create({
     container: {
         padding: 50,
     },
+    inputContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    textInput: {
+        borderWidth: 1,
+        borderColor: 'black',
+        width: '80%',
+        marginRight: 8,
+        padding: 8
+    },
+    goalsContainer: {
+        flex: 3
+    }
 });
